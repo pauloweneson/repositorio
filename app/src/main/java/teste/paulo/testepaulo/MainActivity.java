38421d951e57b580e152501ec5b4d3ab57589d2b
1522130008 <paulowschaves@gmail.com>
2017-08-09 21:57:58 -0300

    Comitando alterações fim de aula

--- app/src/main/java/teste/paulo/testepaulo/MainActivity.java
+++ app/src/main/java/teste/paulo/testepaulo/MainActivity.java
@@ -1,13 +1,56 @@
 package teste.paulo.testepaulo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public interface Teste {
+        public abstract void seila();
+        public void test();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Teste y = new Teste(){
+            @Override
+            public void seila() {
+
+            }
+
+            @Override
+            public void test() {
+
+            }
+        };
+        Teste z = new Teste(){
+            @Override
+            public void seila() {
+
+            }
+
+            @Override
+            public void test() {
+
+            }
+        };
+        Button x = (Button) findViewById(R.id.btnCadastrar);
+        x.setOnClickListener(new View.OnClickListener(){
+            public void onCLick (View view){
+                Intent it = new Intent(MainActivity.this, TelaInicial.class);
+                startActivity(it);
+            }
+        });
+    }
+
+    public void executaEntrar(View parametro){
+        Toast.makeText(this, "Texto de teste", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/teste/paulo/testepaulo/TelaInicial.java
+++ app/src/main/java/teste/paulo/testepaulo/TelaInicial.java
@@ -2,6 +2,7 @@ package teste.paulo.testepaulo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaInicial extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class TelaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+    }
+    public void voltar (View v){
+        finish();
     }
 }
